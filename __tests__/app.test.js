02618f85d4666d0d4ca76ca40af9bbb801ea29cd
SarahRector <rector.sarah.r@gmail.com>
2020-09-21 16:48:27 -0700

    added color routes and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,9 +15,21 @@ describe('createResponse', () => {
     expect(response.text).toEqual('hi there again');
   });
 
-  // it('returns HTML with an h1 and the word red', async() => {
-  //   const response = await request(app)
-  //     .get('/red');
-  //   expect()
-  // })
+  it('returns HTML with an h1 and the word red', async() => {
+    const response = await request(app)
+      .get('/red');
+    expect(response.text).toEqual('<html><body><h1>red</h1></body></html>');
+  });
+
+  it('returns HTML with an h1 and the word green', async() => {
+    const response = await request(app)
+      .get('/green');
+    expect(response.text).toEqual('<html><body><h1>green</h1></body></html>');
+  });
+
+  it('returns HTML with an h1 and the word blue', async() => {
+    const response = await request(app)
+      .get('/blue');
+    expect(response.text).toEqual('<html><body><h1>blue</h1></body></html>');
+  });
 });

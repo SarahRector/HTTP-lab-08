b7e30800ced532414824b88c2489a2c12887cbff
SarahRector <rector.sarah.r@gmail.com>
2020-09-21 16:30:18 -0700

    added / and /echo routes and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,22 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('returns hi', async() => {
+    const response = await request(app)
+      .get('/');
+    expect(response.text).toEqual('hi');
+  });
 
+  it('/echo returns plain text with the response body', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send('hi there again');
+    expect(response.text).toEqual('hi there again');
+  });
+
+  // it('returns HTML with an h1 and the word red', async() => {
+  //   const response = await request(app)
+  //     .get('/red');
+  //   expect()
+  // })
 });
